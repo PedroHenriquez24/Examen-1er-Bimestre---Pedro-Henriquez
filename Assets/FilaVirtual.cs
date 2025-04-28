04fc56d66ba66f7604b0ad86a4373298ae49491e
Pedro <pedrohenriiquez.2008@gmail.com>
2025-04-28 12:17:19 -0300

    Con extras

--- Assets/FilaVirtual.cs
+++ Assets/FilaVirtual.cs
@@ -12,9 +12,13 @@ public class FilaVirtual : MonoBehaviour
     {
         if (tipoEntrada == "b")
         {
-            if (cantEntrada > 0 & cantEntrada < 5)
+            if (cantEntrada > 0 & cantEntrada < 3)
             {
                 Debug.Log($"Tipo de entrada: Básico - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 800}");
+            }
+            else if (cantEntrada >= 3)
+            {
+                Debug.Log($"Tipo de entrada: Básico - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 800*0.9}");
             }
             else
             {
@@ -23,9 +27,13 @@ public class FilaVirtual : MonoBehaviour
         }
         else if (tipoEntrada == "n")
         {
-            if (cantEntrada > 0 & cantEntrada < 7)
+            if (cantEntrada > 0 & cantEntrada < 3)
             {
                 Debug.Log($"Tipo de entrada: Normal - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 1200}");
+            }
+            else if (cantEntrada >= 3)
+            {
+                Debug.Log($"Tipo de entrada: Normal - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 1200 * 0.9}");
             }
             else
             {
@@ -34,17 +42,28 @@ public class FilaVirtual : MonoBehaviour
         }
         else if (tipoEntrada == "p")
         {
-            if (cantEntrada > 0)
+            if (cantEntrada > 0 & cantEntrada < 3)
             {
                 if (premium == true)
                 {
-                    Debug.Log($"Tiene un recargo de ${cantEntrada * 2000 * 1.15 - cantEntrada * 2000} por prioridad en la compra");
-                    Debug.Log($"Tipo de entrada: Premium - Cantidad: {cantEntrada} - Precio total: ${(cantEntrada * 2000)*1.15}");
+                    Debug.Log($"Tiene un recargo de ${cantEntrada * 2000 * 1.15 - cantEntrada * 2000} por prioridad en la compra. Tipo de entrada: Premium - Cantidad: {cantEntrada} - Precio total: ${(cantEntrada * 2000)*1.15}");
 
                 }
                 else
                 {
                     Debug.Log($"Tipo de entrada: Premium - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 2000}");
+                }
+            }
+            else if (cantEntrada >= 3)
+            {
+                if (premium == true)
+                {
+                    Debug.Log($"Tiene un recargo de ${cantEntrada * 2000 * 1.15 - cantEntrada * 2000} por prioridad en la compra. Tipo de entrada: Premium - Cantidad: {cantEntrada} - Precio total: ${(cantEntrada * 2000*0.9) + cantEntrada*2000 * 1.15 - cantEntrada*2000}");
+
+                }
+                else
+                {
+                    Debug.Log($"Tipo de entrada: Premium - Cantidad: {cantEntrada} - Precio total: ${cantEntrada * 2000*0.9}");
                 }
             }
             else
